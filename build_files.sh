850fc56d364ad16f5ea8fbfa2081051eb159c6e4
sajad <sajadmji21@gmail.com>
2024-10-15 12:25:08 +0530

    vo

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Upgrade pip and install requirements
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Run collectstatic
-python3.9 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
