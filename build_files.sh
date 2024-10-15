9149d883ed6b6921cc582b88e6738df57138971e
sajad <sajadmji21@gmail.com>
2024-10-15 12:07:22 +0530

    Update build script and Vercel config

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-pip install -r requirements.txt
+
+# Upgrade pip and install requirements
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+
+# Run collectstatic
 python3.9 manage.py collectstatic --noinput
